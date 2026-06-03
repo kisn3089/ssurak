5acaa5ce66c2a145e98c8925459d96c4a5fa2c54
Insung Hwang <kisn3089@naver.com>
2026-06-04 00:07:54 +0900

    fix(console)/로그아웃 시 쿠키의 token 제거되지 않는 현상 수정 (#62)

--- apps/console/src/providers/OrderdeskAuthProvider.tsx
+++ apps/console/src/providers/OrderdeskAuthProvider.tsx
@@ -3,8 +3,8 @@
 import AuthenticationProvider from "@spaceorder/auth/providers/AuthenticationProvider";
 import { COOKIE_TABLE } from "@spaceorder/db/constants";
 import { useRouter } from "next/navigation";
-import { deleteCookie } from "cookies-next";
 import AxiosInterceptor from "@/lib/AxiosInterceptor";
+import { clearServerCookie } from "@/app/common/servers/cookies";
 
 export function OrderdeskAuthProvider({
   children,
@@ -14,13 +14,22 @@ export function OrderdeskAuthProvider({
   const router = useRouter();
 
   const signOut = () => {
-    deleteCookie(COOKIE_TABLE.ACCESS_TOKEN);
-    deleteCookie(COOKIE_TABLE.REFRESH);
-    router.push("/signin");
+    void (async () => {
+      try {
+        await Promise.all([
+          clearServerCookie(COOKIE_TABLE.ACCESS_TOKEN),
+          clearServerCookie(COOKIE_TABLE.REFRESH),
+        ]);
+      } catch (error) {
+        console.error("Error occurred while clearing cookies:", error);
+      } finally {
+        router.push("/signin");
+      }
+    })();
   };
 
   return (
-    <AuthenticationProvider clientSignOut={signOut}>
+    <AuthenticationProvider serverSignOut={signOut}>
       <AxiosInterceptor>{children}</AxiosInterceptor>
     </AuthenticationProvider>
   );
